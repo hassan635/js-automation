7729ad38c977278ea01dbd6d5b6a14f34fb4a216
hassan635 <hassan.a.tariq@gmail.com>
2020-07-08 20:59:45 +1000

    added search to google navigation spec

--- cypress/integration/examples/google_navigation.spec.js
+++ cypress/integration/examples/google_navigation.spec.js
@@ -1,5 +1,7 @@
 describe("Google test", () =>{
     it("can navigate to google", () =>{
         cy.visit("https://www.google.com/")
+        cy.get("input[name='q']").type("Allah")
+        cy.get("input[aria-label='Google Search']").click()
     });
 });
