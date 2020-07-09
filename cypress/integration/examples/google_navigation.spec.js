7a4a411fe7a998c6a7743a5e66b810f483085b3a
hassan635 <hassan.a.tariq@gmail.com>
2020-07-09 22:07:31 +1000

    Close after test execution

--- cypress/integration/examples/google_navigation.spec.js
+++ cypress/integration/examples/google_navigation.spec.js
@@ -2,5 +2,6 @@ describe("Google test", () =>{
     it("can navigate to google", () =>{
         cy.visit("https://www.google.com/")
         cy.get("input[name='q']").type("Allah").type("\r")
+        cy.close()
     });
 });
