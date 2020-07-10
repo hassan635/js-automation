da0e443f7ad03ae5c0fafe2506ae7147765d23b4
hassan635 <hassan.a.tariq@gmail.com>
2020-07-10 22:41:10 +1000

    deprecated close

--- cypress/integration/examples/google_navigation.spec.js
+++ cypress/integration/examples/google_navigation.spec.js
@@ -2,6 +2,5 @@ describe("Google test", () =>{
     it("can navigate to google", () =>{
         cy.visit("https://www.google.com/")
         cy.get("input[name='q']").type("Allah").type("\r")
-        cy.close()
     });
 });
