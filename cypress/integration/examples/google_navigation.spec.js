248dd60941b5ebd8330dafc3076cf3f474ec47cf
hassan635 <hassan.a.tariq@gmail.com>
2020-07-11 16:06:29 +1000

    Added assertion to google navigation test

--- cypress/integration/examples/google_navigation.spec.js
+++ cypress/integration/examples/google_navigation.spec.js
@@ -1,6 +1,6 @@
 describe("Google test", () =>{
     it("can navigate to google", () =>{
         cy.visit("https://www.google.com/")
-        cy.get("input[name='q']").type("Allah").type("\r")
+        cy.get("input[name='q']").type("Allah").type("\r").should("have.value", "Allah-u-Akbar")
     });
 });
